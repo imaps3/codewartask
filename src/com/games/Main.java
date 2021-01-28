c82d910a69d9c06b2c4104b2184a8ea2c17e8388
jay <jay@localhost.localdomain>
2021-01-29 03:35:31 +0400

    added method to square single digits in a num

--- src/com/games/Main.java
+++ src/com/games/Main.java
@@ -14,4 +14,16 @@ public class Main {
         }
         return count;
         }
+    public static int squareDigits(int n) {
+
+        String result = "";
+
+        while (n != 0) {
+            int digit = n % 10 ;
+            result = digit*digit + result ;
+            n /= 10 ;
+        }
+
+        return Integer.parseInt(result) ;
+    }
 }
